91640033ffbe73b0eb4e741b9f71021800055d5d
David Scott <dave.scott@citrix.com>
2015-03-10 09:43:42 +0000

    test: I'm not interested in code coverage of the test log functions
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- lib_test/log.ml
+++ lib_test/log.ml
@@ -1,3 +1,5 @@
+(*BISECT-IGNORE-BEGIN*)
 let debug fmt = Printf.ksprintf (fun s -> print_endline s) fmt
 let info  fmt = Printf.ksprintf (fun s -> print_endline s) fmt
 let error fmt = Printf.ksprintf (fun s -> print_endline s) fmt
+(*BISECT-IGNORE-END*)
