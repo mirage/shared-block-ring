f250f6695c02ce814fcbe0764e893c113a9cc4d3
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-10-06 16:24:56 +0100

    Make all the Log functions return Lwt threads
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- lib_test/log.ml
+++ lib_test/log.ml
@@ -1,10 +1,10 @@
 (*BISECT-IGNORE-BEGIN*)
-let debug fmt = Printf.ksprintf (fun s -> print_endline s) fmt
-let info  fmt = Printf.ksprintf (fun s -> print_endline s) fmt
-let error fmt = Printf.ksprintf (fun s -> print_endline s) fmt
+let debug fmt = Lwt_log.debug_f fmt
+let info  fmt = Lwt_log.info_f fmt
+let error fmt = Lwt_log.error_f fmt
 let verbose = ref false
 let trace list =
   if !verbose
   then info "%s" (Sexplib.Sexp.to_string (Shared_block.S.sexp_of_traced_operation_list list))
-  else ()
+  else Lwt.return ()
 (*BISECT-IGNORE-END*)
